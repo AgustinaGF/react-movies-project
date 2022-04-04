2a61164aa5e22f5709cb82c16830d4638ccf1413
Agustina Gomez Fernandez <agustinagomez385@gmail.com>
2022-04-04 18:23:44 -0300

    searchBar

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -3,18 +3,28 @@ import styles from "./MovieGrid.module.css";
 import { useEffect, useState } from "react";
 import get from "../utils/httpClients";
 import Spinner from "./Spinner";
+import useQuery from "./hooks/useQuery";
 
 export default function MoviesGrid() {
 	const [movies, setMovies] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
 
+	const query = useQuery();
+	// esto nos va a traer el valor de search que pasamos en el parametro
+	const search = query.get("search");
+	console.log(search);
+
 	useEffect(() => {
 		setIsLoading(true);
-		get("/discover/movie").then((data) => {
+		// aca lo que hace es buscar por el valor de busqueda que paso y si no hay busqueda me va a traer todas las peliculas
+		const searchUrl = search
+			? "/search/movie?query=" + search
+			: "/discover/movie";
+		get(searchUrl).then((data) => {
 			setMovies(data.results);
 			setIsLoading(false);
 		});
-	}, []);
+	}, [search]);
 	if (isLoading) {
 		return <Spinner />;
 	}
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,32 @@
 import styles from "./SearchBar.module.css";
+import { useState, useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
+import { useHistory } from "react-router-dom";
+import useQuery from "./hooks/useQuery";
 
 export default function SearchBar() {
+	const [searchText, setSearchText] = useState("");
+	const history = useHistory();
+	const query = useQuery();
+	const search = query.get("search");
+
+	// funcionalidad de la barra de busqueda
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		history.push("/?search=" + searchText);
+	};
+	useEffect(() => {
+		setSearchText(search || "");
+	}, [search]);
+
 	return (
-		<form className={styles.searchContainer}>
+		<form className={styles.searchContainer} onSubmit={handleSubmit}>
 			<div className={styles.searchBox}>
-				<input className={styles.searchInput}></input>
+				<input
+					className={styles.searchInput}
+					value={searchText}
+					onChange={(e) => setSearchText(e.target.value)}
+				></input>
 				<button className={styles.searchButton} type="submit">
 					{/* el icono lo pongo como componente */}
 					<FaSearch size={20} />
